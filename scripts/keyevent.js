a160d0d770a2249f1230cab81b7747cdef61a451
Enzo Vandepoele <vandepoele.e@example.com>
2023-12-27 11:33:17 +0100

    Bug produit

--- scripts/keyevent.js
+++ scripts/keyevent.js
@@ -1,4 +1,4 @@
-var container = document.querySelector(".container");
+var container = document.querySelector(".container-main");
 
 document.addEventListener('keydown', function(event) {
   if(event.key === 'F' || event.key === 'f') {
